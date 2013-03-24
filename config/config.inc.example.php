4db670567ad4c2430363c264a4dac95b5f362268
chamoAt <florianrusch92@gmail.com>
2013-03-24 04:18:00 +0100

    Hide the login details for the database

---
+++ config/config.inc.example.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Entwickler-Modus
+ */
+define ('DEV', true);
+
+
+
+/*
+ * Database
+ */
+define('DB_NAME', 'DBNAME');
+define('DB_USER', 'USER');
+define('DB_PASSWORD', 'PASSWORT');
+define('DB_HOST', 'localhost');
+
+?>
